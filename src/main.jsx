1a0b6590c6fa2631cb133d186827cf7e77da10b9
Alexanderkov0 <foxnation7@gmail.com>
2025-05-14 22:22:03 +0300

    init vite

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const rootEl = document.getElementById('root');
+
+const app = createRoot(rootEl);
+
